2b39542d439d37f517e1598c456ed593b0c1c697
MuneesRaja <muneesraja@pop-os.localdomain>
2020-06-05 14:31:16 +0530

    Initial commit for monster search

---
+++ precache-manifest.0c19a8afdbe6c17d9f2a8480db22a9e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdc85feba90604afa93b06753d2e6d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e22e2e313f47fa4c47d1",
+    "url": "/static/css/main.bb7befd4.chunk.css"
+  },
+  {
+    "revision": "06f740c4d0bf147429df",
+    "url": "/static/js/2.a9b70e73.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9b70e73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e22e2e313f47fa4c47d1",
+    "url": "/static/js/main.e71b3842.chunk.js"
+  },
+  {
+    "revision": "65170f7b8d30dc8dfcae",
+    "url": "/static/js/runtime-main.3f8a155f.js"
+  }
+]);
